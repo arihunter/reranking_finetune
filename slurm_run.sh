d102d27e5e47e9fb07680e8669c1eef8073e6ea5
Arihant Barjatya <61943856+arihunter@users.noreply.github.com>
2024-11-15 15:07:39 -0500

    slurm_update

--- slurm_run.sh
+++ slurm_run.sh
@@ -6,17 +6,16 @@
 #SBATCH --mem-per-cpu=11G # Important to enable "mix" use of GPUs across cluster users
 #SBATCH --partition=XXXXX
 #SBATCH --gres=gpu:8 # Adjust number of GPUs here
-#SBATCH --output=shared_storage/sourab/temp/logs/%x-%j.out
-#SBATCH --err=shared_storage/sourab/temp/logs/%x-%j.err
+#SBATCH --output=/home/abarjatya_umass_edu/intrepretability/rerank_finetune/temp/logs/%x-%j.out
+#SBATCH --err=/home/abarjatya_umass_edu/intrepretability/rerank_finetune/temp/logs/%x-%j.err
 
 set -x -e
 
 # CHANGE HERE THE CONDA EVN AND ANY STARTUP SCRIPTS
-source ~/sourab/.bashrc
-source shared_storage/sourab/miniconda3/etc/profile.d/conda.sh
-conda activate hf
-cd shared_storage/sourab/DHS-LLM-Workshop/code_assistant/training
-git pull
+source /home/abarjatya_umass_edu/intrepretability/rerank_finetune/.bashrc
+source /home/abarjatya_umass_edu/intrepretability/rerank_finetune/miniconda3/etc/profile.d/conda.sh
+conda activate finetune_env
+cd /home/abarjatya_umass_edu/intrepretability/rerank_finetune
 
 # have the below in case of debugging nccl issues such as nccl timeout.
 # export NCCL_DEBUG=INFO
@@ -33,20 +32,10 @@ export NCCL_ASYNC_ERROR_HANDLING=1
 # export NCCL_NSOCKS_PERTHREAD=1
 # export CUDA_LAUNCH_BLOCKING=1
 
-# AWS specific
-# export NCCL_PROTO=simple
-# export RDMAV_FORK_SAFE=1
-# export FI_EFA_FORK_SAFE=1
-# export FI_EFA_USE_DEVICE_RDMA=1
-# export FI_PROVIDER=efa
-# export FI_LOG_LEVEL=1
-# export NCCL_IB_DISABLE=1
-# export NCCL_SOCKET_IFNAME=ens
-
 echo "START TIME: $(date)"
 
 # CHANGE TO CUMMULATIVELY LOG OUTPUTS
-LOG_PATH="main_log.txt"
+LOG_PATH="/home/abarjatya_umass_edu/intrepretability/rerank_finetune/main_log.txt"
 
 GPUS_PER_NODE=8
 NNODES=$SLURM_NNODES
@@ -58,7 +47,7 @@ MASTER_PORT=6000
 
 # OTHER LAUNCHERS CAN BE USED HERE
 export LAUNCHER="accelerate launch \
-    --config_file configs/fsdp_config.yaml \
+    --config_file /home/abarjatya_umass_edu/intrepretability/rerank_finetune/ds_config.yaml \
     --main_process_ip $MASTER_ADDR \
     --main_process_port $MASTER_PORT \
     --machine_rank \$SLURM_PROCID \
@@ -68,7 +57,7 @@ export LAUNCHER="accelerate launch \
 # Note: it is important to escape `$SLURM_PROCID` since we want the srun on each node to evaluate this variable
 
 export PROGRAM="\
-train.py \
+reranker_train.py \
     --model_name "meta-llama/Llama-3.1-8B" \
     --dataset_name Tevatron/msmarco-passage \
     --rerank_max_len 256 \
@@ -76,34 +65,21 @@ train.py \
     --logging_steps 500 \
     --eval_steps 100 \
     --save_steps 20000 \
-    --bf16 True \
     --packing True \
-    --output_dir "/shared_storage/sourab/experiments/full-finetune-llama-chat-asst" \
-    --per_device_train_batch_size 1 \
+    --output_dir reranker_msmarco  \
+    --dataloader_num_workers 2
+    --num_train_epochs 5 \
+    --train_group_size 8 \
+    --learning_rate 5e-6 \
+    --per_device_train_batch_size 16 \
     --gradient_accumulation_steps 1 \
-    --dataset_text_field "content" \
     --use_gradient_checkpointing True \
-    --learning_rate 5e-5  \
     --lr_scheduler_type "cosine" \
     --weight_decay 0.01 \
     --warmup_ratio 0.03 \
     --use_flash_attn True \ 
-    --output_dir reranker_msmarco \
+    --dataloader_num_workers 2 
 "
-
-CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port 29001 examples/reranker/reranker_train.py \
-  --output_dir reranker_msmarco \
-  --model_name_or_path bert-base-uncased \
-  --save_steps 20000 \
-  --dataset_name Tevatron/msmarco-passage \
-  --fp16 \
-  --per_device_train_batch_size 16 \
-  --train_group_size 8 \
-  --learning_rate 5e-6 \
-  --rerank_max_len 256 \
-  --num_train_epochs 5 \
-  --logging_steps 500 \
-  --dataloader_num_workers 2
 
 export CMD="$LAUNCHER $PROGRAM"
 
